31a7369a06e0c8a9b4dc2d2fe816b3b46350c1fb
Nathan <nslee333@gmail.com>
2024-01-08 15:53:03 -0800

    added app and templates folder - basic testing

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import url_for, render_template
 
 app = Flask(__name__)
+# url_for('static', filename='index.html')
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/app")
+def index():
+    return render_template('index.html', message=None)
