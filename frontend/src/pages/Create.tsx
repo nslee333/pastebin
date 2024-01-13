d0b575d696857e6755421aa5e37f4b18e4a23822
Nathan <nslee333@gmail.com>
2024-01-13 14:54:38 -0800

    wip

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,1 +1,13 @@
-export {}
+import '../styles/App.css';
+
+function Login() {
+  return (
+    <div className="">
+      <header className="bg-black">
+        Hello
+      </header>
+    </div>
+  );
+}
+
+export default Login;
