01e35d70f6b8cd5d05e23903d106f6e122961be4
Nathan <nslee333@gmail.com>
2024-01-13 16:28:39 -0800

    formatting

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,36 +5,39 @@ function Home() {
     <div className="body">
       <header className="bg-[#666] flex justify-center h-[8rem]">
         <div className="text-[#fff] flex items-center font-bold text-3xl w-[50%]">
+          
           <div className="text-center">
             Pastebin
           </div>
+          
         </div>
       </header>
 
-    <div className="flex justify-center m-[2rem]">
-      <div className="w-[50%] h-[70vh] p-[2rem] bg-[#555] rounded-lg text-[#fff]">
-        <div className="bg-[#333] rounded-lg h-[60vh] p-[1rem]">
-          <div className="bg-[#555] m-[1rem] p-[.5rem] rounded">
-            <div className="bg-[#222] rounded p-[.25rem]">
-              Title
+      <div className="flex justify-center m-[2rem]">
+        <div className="w-[50%] h-[70vh] p-[2rem] bg-[#555] rounded-lg text-[#fff]">
+          <div className="bg-[#333] rounded-lg h-[60vh] p-[1rem]">
+            
+            <div className="bg-[#555] m-[1rem] p-[.5rem] rounded">
+              <div className="bg-[#222] rounded p-[.25rem]">
+                Title
+              </div>
             </div>
-          </div>
-          <div className="bg-[#555] m-[1rem] h-[40vh] p-[.75rem] rounded">
-            <div className="bg-[#222] rounded p-[.25rem] h-[37vh]">
-              Text Area
+            
+            <div className="bg-[#555] m-[1rem] h-[40vh] p-[.75rem] rounded">
+              <div className="bg-[#222] rounded p-[.25rem] h-[37vh]">
+                Text Area
+              </div>
             </div>
-          </div>
-          <div className="flex justify-end">
-            <button className="bg-[#2ec116] font-bold w-[10rem] h-[2rem] rounded m-[1rem] align-right">
-              Create Paste 
-            </button>
+
+            <div className="flex justify-end">
+              <button className="bg-[#2ec116] font-bold w-[10rem] h-[2rem] rounded m-[1rem] align-right">
+                Create Paste 
+              </button>
+            </div>
+
           </div>
         </div>
       </div>
-    </div>
-
-
-
     </div>
   );
 }
